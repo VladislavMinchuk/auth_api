54e1a1e2656add63b7c4e9e30853c76d207be693
Vladislav Minchuk <vladislav.minchuk@p2h.com>
2020-07-02 11:50:11 +0300

    Login Strategy upddate

--- config/passport.js
+++ config/passport.js
@@ -10,16 +10,16 @@ module.exports = (passport) => {
     },
     async (email, password, done) => {
       try {
-        console.log(password, ' passport');
-        console.log(await hashService.create(password));
-        console.log(await hashService.compare(password, await hashService.create(password)));
-  
         const user = await User.findOne({ email });
-        if (!user || hashService.compare(password, user.password)) {
+        const isValidPassword = await hashService.compare(password, user.password);
+
+        if (!user || !isValidPassword) {
           return done(null, false, { message: 'User is not found or password is invalid' });
         }
+
+        return done(null, user);
       } catch (err) {
-          return done(err);
+        return done(err);
       }
     }
   ));
